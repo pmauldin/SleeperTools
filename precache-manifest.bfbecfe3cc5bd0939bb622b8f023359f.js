fa26fd810b7f64cfbda844f47d38b59e741d196b
Peter Mauldin <peter@poologics.com>
2020-06-27 15:24:50 -0500

    Updates

---
+++ precache-manifest.bfbecfe3cc5bd0939bb622b8f023359f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1428e82d73052b583f8223f392d6f37",
+    "url": "/sleeper-tools/index.html"
+  },
+  {
+    "revision": "243db7e4c37899e996ee",
+    "url": "/sleeper-tools/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "128d7d3fb36c9bb62287",
+    "url": "/sleeper-tools/static/js/2.89741ae5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sleeper-tools/static/js/2.89741ae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243db7e4c37899e996ee",
+    "url": "/sleeper-tools/static/js/main.b31fe336.chunk.js"
+  },
+  {
+    "revision": "28c2ded86a08013d0d65",
+    "url": "/sleeper-tools/static/js/runtime-main.ac501f59.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sleeper-tools/static/media/logo.5d5d9eef.svg"
+  }
+]);
